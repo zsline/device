f96e6c7f8d9264b4e641e562531b931e7fe2a08a
zsline <sergei.dereka@gmail.com>
2022-01-26 18:36:04 +0200

    slider

--- js/main.js
+++ js/main.js
@@ -2,4 +2,13 @@ $(function () {
 $('.menu__btn').on('click', function () {
   $('.menu__box').toggleClass('menu__box-active');
 });
+  $('.head__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: true,
+    fade: true,
+  });
 });
